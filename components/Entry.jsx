8dc943d76fcde5b77e3295f5d69d8b1ee0a25aaa
pyry.rautalin@gmail.com <RANDOM_EMAIL>
2025-04-08 22:27:32 +0300

    First test journal entry component  with hardcoded data

---
+++ components/Entry.jsx
@@ -0,0 +1,12 @@
+export const Entry = () => (
+        <article className="journal-entry">
+            <div className="entry-image-container">
+            <img className="journal-entry-image" src="salikuvatornio.jpg" alt="picture of a dimly lit gym" />
+            </div>
+            <div>
+                <img className="category" src="kettlebell.png" alt="kettlebell icon" />
+                <span>Tornio Bodycenter</span>
+                <a href="https://www.bckgsalit.fi/bodycenter-tornio/"></a> 
+            </div>
+        </article>
+)
