b949fd0f4eb30660399e13464ced0ba724babf7f
pyry.rautalin@gmail.com <pypy@DESKTOP-B9BO8U0>
2025-04-08 19:49:20 +0300

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => (
+    <div className="header">
+        <div className="headerbar">
+        <img className="logo" src="dumbbell.png" alt="white dumbell logo" />
+        <span>My gym journal</span>
+        </div>
+        
+    </div>
+
+)
